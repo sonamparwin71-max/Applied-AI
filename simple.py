d06564b5ff36dc138d6d7556daac51f71464a904
sonamparwin71-max <sonamparwin71@gmail.com>
2026-06-01 12:36:33 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,7 @@
+p = float(input("Enter Principal Amount: "))
+r = float(input("Enter Rate of Interest: "))
+t = float(input("Enter Time (in years): "))
+
+s= (p * r * t) / 100
+
+print("Simple Interest =", s)
