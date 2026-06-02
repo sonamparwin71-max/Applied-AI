e7b6508ede6fefff1d1da6868deb590d847c075b
sonamparwin71-max <sonamparwin71@gmail.com>
2026-06-02 11:26:12 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+a=int(input("Enter 1st Number:"))
+b=int(input("Enter 2nd Number:"))
+c=int(input("Enter 3rd Number:"))
+d=int(input("Enter 4th Number:"))
+
+average=(a+b+c+d)/4
+print("Average of 4 numbers is:",average)
