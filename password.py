3ee928754f8032234d78aa41f0caa8d4146a6e78
sonamparwin71-max <sonamparwin71@gmail.com>
2026-06-03 12:21:56 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,8 @@
+correct_password = "sonam123"
+
+password = input("Enter Password: ")
+
+if password == correct_password:
+    print("Password is Correct")
+else:
+    print("Password is Incorrect")
